c179e506aa408df3a97970461724f1e2ce209cfb
Andrew Smith <drewsmits@gmail.com>
2011-09-10 00:46:06 -0700

    Added test for UIView+Insert

---
+++ CatKitUITests/UIViewInsertTests.h
@@ -0,0 +1,16 @@
+//
+//  UIViewInsertTests.h
+//  CatKit
+//
+//  Created by Andrew Smith on 9/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import <UIKit/UIKit.h>
+
+@interface UIViewInsertTests : SenTestCase {
+    UIView *view;
+}
+
+@end
---
+++ CatKitUITests/UIViewInsertTests.m
@@ -0,0 +1,45 @@
+//
+//  UIViewInsertTests.m
+//  CatKit
+//
+//  Created by Andrew Smith on 9/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "UIViewInsertTests.h"
+#import "UIView+Insert.h"
+
+@implementation UIViewInsertTests
+
+
+- (void)setUp {
+    [super setUp];
+    
+    view = [[UIView alloc] initWithFrame:CGRectZero];
+}
+
+- (void)tearDown {
+    // Tear-down code here.
+    [view release];
+    
+    [super tearDown];
+}
+
+- (void)testInsertSubviewAtBack {
+    
+    UIView *container = [[UIView alloc] initWithFrame:CGRectZero];
+    UIView *view1     = [[UIView alloc] initWithFrame:CGRectZero];
+    UIView *view2     = [[UIView alloc] initWithFrame:CGRectZero];
+    
+    [container addSubview:view1];
+    [container addSubview:view2];
+
+    [container insertSubviewAtBack:view];
+    
+    BOOL pass = ([container.subviews indexOfObject:view] == 0);
+     
+     STAssertTrue(pass, @"insertSubviewAtBack should put subview last");
+    
+}
+
+@end
