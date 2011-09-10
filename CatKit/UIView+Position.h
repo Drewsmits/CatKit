3ead9ae95a56c3c8d77310914bc870b9f6ccbb33
Andrew Smith <drewsmits@gmail.com>
2011-09-09 19:46:45 -0700

    Added README, first category on UIView

---
+++ CatKit/UIView+Position.h
@@ -0,0 +1,13 @@
+//
+//  UIView+Position.h
+//  CatKit
+//
+//  Created by Andrew Smith on 9/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+
+
+@interface UIView (Position)
+
+@end
---
+++ CatKit/UIView+Position.m
@@ -0,0 +1,13 @@
+//
+//  UIView+Position.m
+//  CatKit
+//
+//  Created by Andrew Smith on 9/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "UIView+Position.h"
+
+@implementation UIView (Position)
+
+@end
