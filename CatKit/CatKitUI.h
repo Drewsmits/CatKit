758a5fee1b8768826ffb966aa7d32595f808a4e1
Andrew B. Smith <drewsmits@gmail.com>
2012-07-04 01:24:39 -0700

    Added a bunch of image manipulation stuff

--- CatKit/CatKitUI.h
+++ CatKit/CatKitUI.h
@@ -12,4 +12,17 @@
 #import "UIView+Position.h"
 #import "UIView+Insert.h"
 
+void RandomlyColorSubviewsOfView(UIView *view) {
+    for (UIView *sv in view.subviews) {
+        
+        CGFloat r = (arc4random() % 255) / 255.0;
+        CGFloat g = (arc4random() % 255) / 255.0;
+        CGFloat b = (arc4random() % 255) / 255.0;
+        
+        sv.backgroundColor = [UIColor colorWithRed:r green:g blue:b alpha:0.5];
+        
+        RandomlyColorSubviewsOfView(sv);
+    }
+}
+
 #endif
---
+++ CatKit/UIImage+Orientation.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+Orientation.h
+//  CatKit
+//
+//  Created by Andrew Smith on 6/30/12.
+//  Copyright (c) 2012 Andrew B. Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Orientation)
+
+- (UIImage *)fixOrientation;
+
+@end
